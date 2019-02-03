87738cc6e48f4d8c327512bf7ec8fd392c5e6399
Berk Özbalcı <berkozbalci@gmail.com>
2019-02-03 03:11:50 +0300

    End of Chapter 14: Chunks of Bytecode

---
+++ README.md
@@ -0,0 +1,5 @@
+# clox
+
+This is the bytecode virtual machine for Lox, the programming language of the [Crafting Interpreters](http://www.craftinginterpreters.com/) book.
+
+Currently a work in progress.
---
+++ tool/driver/main.c
@@ -0,0 +1,19 @@
+#include <clox/common.h>
+#include <clox/chunk.h>
+#include <clox/debug.h>
+
+int main(int argc, const char *argv[]) {
+    Chunk chunk;
+    initChunk(&chunk);
+
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
+
+
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
+    return 0;
+}
