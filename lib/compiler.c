8c180a6d79a1184a83d64c832cd4cfec7f187e13
Berk Özbalcı <berkozbalci@gmail.com>
2019-02-03 05:55:12 +0300

    End of Chapter 16: Scanning on Demand

---
+++ lib/compiler.c
@@ -0,0 +1,24 @@
+#include <clox/compiler.h>
+
+#include <clox/common.h>
+#include <clox/scanner.h>
+
+#include <stdio.h>
+
+void compile(const char *source) {
+    initScanner(source);
+    int line = -1;
+
+    for (;;) {
+        Token token = scanToken();
+        if (token.line != line) {
+            printf("%4d ", token.line);
+            line = token.line;
+        } else {
+            printf("   | ");
+        }
+        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+
+        if (token.type == TOKEN_EOF) break;
+    }
+}
--- lib/vm.c
+++ lib/vm.c
@@ -1,51 +1,52 @@
 #include <clox/vm.h>
 
 #include <clox/common.h>
+#include <clox/compiler.h>
 #include <clox/debug.h>
 
 #include <stdio.h>
 
-static void resetStack(VM *vm) {
-    vm->stackTop = vm->stack;
+VM vm;
+
+static void resetStack() {
+    vm.stackTop = vm.stack;
 }
 
-void initVM(VM *vm) {
-    resetStack(vm);
+void initVM() {
+    resetStack();
 }
 
-void freeVM(VM *vm) {
+void freeVM() {
 
 }
 
-static InterpretResult run(VM *vm) {
-#define READ_BYTE() (*vm->ip++)
-#define READ_CONSTANT() (vm->chunk->constants.values[READ_BYTE()])
-#define PUSH(value) (push(vm, value))
-#define POP() (pop(vm))
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
 #define BINARY_OP(op) \
     do { \
-        double b = POP(); \
-        double a = POP(); \
-        PUSH(a op b); \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
     } while (false)
 
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
         printf("          ");
-        for (Value *slot = vm->stack; slot < vm->stackTop; slot++) {
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++) {
             printf("[ ");
             printValue(*slot);
             printf(" ]");
         }
         printf("\n");
-        disassembleInstruction(vm->chunk, (int) (vm->ip - vm->chunk->code));
+        disassembleInstruction(vm.chunk, (int) (vm.ip - vm.chunk->code));
 #endif
         uint8_t instruction;
 
         switch (instruction = READ_BYTE()) {
             case OP_CONSTANT: {
                 Value constant = READ_CONSTANT();
-                PUSH(constant);
+                push(constant);
                 printf("\n");
                 break;
             }
@@ -54,11 +55,11 @@ static InterpretResult run(VM *vm) {
             case OP_MULTIPLY: BINARY_OP(*); break;
             case OP_DIVIDE:   BINARY_OP(/); break;
             case OP_NEGATE: {
-                PUSH(-POP());
+                push(-pop());
                 break;
             }
             case OP_RETURN: {
-                printValue(POP());
+                printValue(pop());
                 printf("\n");
                 return INTERPRET_OK;
             }
@@ -68,24 +69,21 @@ static InterpretResult run(VM *vm) {
         }
     }
 #undef BINARY_OP
-#undef POP
-#undef PUSH
 #undef READ_CONSTANT
 #undef READ_BYTE
 }
 
-InterpretResult interpret(VM *vm, Chunk *chunk) {
-    vm->chunk = chunk;
-    vm->ip = vm->chunk->code;
-    return run(vm);
+InterpretResult interpret(const char *source) {
+    compile(source);
+    return INTERPRET_OK;
 }
 
-void push(VM *vm, Value value) {
-    *vm->stackTop = value;
-    vm->stackTop++;
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
 }
 
-Value pop(VM *vm) {
-    vm->stackTop--;
-    return *vm->stackTop;
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
 }
